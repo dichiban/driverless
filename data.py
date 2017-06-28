cda795d42aa1eed8c3383d617f001a4b32cea466
dichiban <daiki.ichiyama@gmail.com>
2017-06-28 23:35:47 +1000

    :star: Added get_latest_image

--- data.py
+++ data.py
@@ -6,8 +6,9 @@ from sklearn.model_selection import train_test_split
 import os
 
 
-IMAGE_LOCATION = "Ch2_001\center"
+IMAGE_LOCATION = "Ch2_001/center"
 DATA_LOCATION = "Ch2_001/final_example.csv"
+
 
 NUM_IMAGES = 4491
 def load_images_from_folder(folder):
@@ -20,8 +21,11 @@ def load_images_from_folder(folder):
     for filename in os.listdir(folder):
         img = cv2.imread(os.path.join(folder, filename))
         if img is not None:
-            resized = cv2.resize(img, (100, 100))
+            #resized = cv2.resize(img, (128, 128))
+            resized = cv2.resize(img[240:480, 0:640], (128, 128))
+            #resized = img[240:480, 0:640]
             images.append(resized)
+
     return np.asarray(images)
 
 
@@ -40,5 +44,15 @@ def load_data():
     y = data_df['steering_angle'].values
 
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)
-    return X_train, X_valid, y_train, y_valid
+    y_train = np.reshape(y_train, (y_train.size, 1))
+    y_valid = np.reshape(y_valid, (y_valid.size, 1))
 
+    return X, y
+    #return X_train, X_valid, y_train, y_valid
+
+def get_latest_image(folder):
+    latest = cv2.imread(os.path.join(folder, max((os.listdir(folder)))))
+    latest_resized = cv2.resize(latest[240:480, 0:640], (128, 128))
+    latest_resized = latest_resized.reshape((1, 128, 128, 3))
+
+    return latest_resized
