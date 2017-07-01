e00796f0f54b2dfa215f3c3facd951e4fa6dfcc7
dichiban <daiki.ichiyama@gmail.com>
2017-07-02 02:34:43 +1000

    :straightruler: un-normalised steering angle

--- drive.py
+++ drive.py
@@ -1,17 +1,36 @@
 from keras.models import model_from_yaml
+import serial
 import data
+import cv2
+import threading
+from time import time
 
-x_train, y_train = data.load_data()
-yaml_file = open('model.yaml', 'r')
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+yaml_file = open('IT_WORKS/model0.yaml', 'r')
 loaded_model_yaml = yaml_file.read()
 yaml_file.close()
 
 model = model_from_yaml(loaded_model_yaml)
-model.load_weights("model_weights.h5")
+model.load_weights('IT_WORKS/model_weights0.h5')
+
+cam = cv2.VideoCapture(0)
+frame = 0
+
+start = time()
+i = 0
 
 while 1:
-    pic = data.get_latest_image("Ch2_001/center")
-
-    y = model.predict(pic)
-
-    print(y)
+    s, im = cam.read()
+    im = data.augment_data(im)
+    im = im.reshape((1, 128, 128, 3))
+    y = model.predict(im)
+    frame += 1
+    y = data.norm_label(y, -1, 1, 1044, 1750)
+    fin = time()
+    if fin - start >= 1:
+        i+=1
+        fps = frame/i
+        print(fps)
+        start = fin
+    ser.write('steer,'+str(y)+"\n")
